f9a31a3ca4534ba366fb0a8321fd2aa7f844ce2c
Karina Davydenko <kada61100@gmail.com>
2023-09-25 00:49:36 +0300

    Основы работы с Map

--- app.js
+++ app.js
@@ -1,46 +1,60 @@
 'use strict';
 
-// Когда создаем какой-то массив данных (билеты на самолет в различные страны)
-const flights = ['Moscow', 'Paris', 'London', 'London', 'Paris'];
-console.log(flights);
-// ['Moscow', 'Paris', 'London', 'London', 'Paris']
+// JS объекты и Maps с точки зрения реализации немного похожи
 
-// Часто встает задача уникализировать такой массив данных.
-// И вот тут становяться полезными Sets.
+//  Работа структуры данных Hash map.
+// Зная, что нам нужно сохранить ключи и значения, будем действовать следующим образом.
+// Мы возьмем ключ и прогоним его через некую Hash-функцию, которая вернет нам значение(bucket).
+// Структура Hash map позволяет нам оптимизировать поиск, добавление и удаление эл-тов.
 
-// Создаем структуру Set(итерируемый объект)
-const setFlights = new Set(flights)
-console.log(setFlights)
-// Set(3) {'Moscow', 'Paris', 'London'}
+// По причине того что JS объекты не совсем четко реализуют структуру Hash map,
+//  у них все-таки поиск медленее, с ES6 и был введен Map(новый тип данных, быстрый поиск по объектам)
+// При этом Map более чистый тип данных. Все таки JS объект имеет прототип, кдючи по умл.
+// В Map все что угодно может быть ключем. Есть свойство size.
+// Map сильно оптимизирован для частых операций. 
+// Если нужно работать с большими ключезначениями. Частыми операциями. И не нужны методы объекта.
 
-console.log(setFlights.size) // 3
-console.log(setFlights.has('Paris')) // true
-console.log(setFlights.has('none')) // false
+const wetherMap = new Map()
+console.log(wetherMap); // Map(0) {size: 0}
 
-setFlights.delete('Paris') //удалит Paris
+// дополнить Map
+wetherMap.set('London', '+10')
+console.log(wetherMap); // Map(1) {'London' => '+10'}
+console.log(wetherMap.set('Moscow', '+5')) // Map(2) {'London' => '+10', 'Moscow' => '+5'}
 
-setFlights.clear() //очистит Set
+wetherMap
+  .set('Toronto', '+28')
+  .set('Minnesota', '+27')
 
-setFlights.add('Paris')
-setFlights.add('Toronto')
-console.log(setFlights)
-// Set(2) {'Paris' 'Toronto'}
+// получение значение 
+console.log(wetherMap.get('Moscow')) // +5
+console.log(wetherMap.get('not')) // undefined
 
-// перебор по Set
-for (const flight of setFlights) {
-  console.log(flight);
-}
+// проверка значения
+console.log(wetherMap.has('Moscow')); // true
+console.log(wetherMap.has('not')) // false
 
-// переобразование Set в массив с помощью ..spred
-const newFlights = [...setFlights]
-console.log(newFlights)
-// (2) ['Paris', 'Toronto']
+// удаление
+console.log(wetherMap.delete('Moscow')) //true
+console.log(wetherMap.delete('not')) // false
+console.log(wetherMap);
+//Map(3) {'London' => '+10', 'Toronto' => '+28', 'Minnesota' => '+27'}
 
+// можем добавлять ключи другого типа
+wetherMap
+  .set(1, 5)
+  .set(1, 6) // перезаписывается пред значение
 
-// понимаем что Set колекции объктов мы не можем уникализировать по одинак. свойствам
-const setObj = new Set([{ a: 1 }, { b: 2 }, { a: 1 }])
-console.log(setObj);
-// Set(3) {{ a: 1 }, { b: 2 }, { a: 1 }}
+// колво свойств
+console.log(wetherMap.size);
 
-console.log(new Set('a a aka aka'));
-// Set(3) {'a', ' ', 'k'}
+// очистка
+console.log(wetherMap.clear()); // undefined
+
+//осторожно с сотвавными типами в ключах
+wetherMap.set({ a: 1 }, '1')
+console.log(wetherMap.get({ a: 1 })); // undefined
+
+const key = [1, 2, 3]
+wetherMap.set(key, [1, 2, 3])
+console.log(wetherMap.get(key)); // [1, 2, 3]
