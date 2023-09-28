fc3828dccda809734609d725d033fad60838d909
Karina Davydenko <kada61100@gmail.com>
2023-09-28 23:51:35 +0300

    WeakMap

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 'use strict'
+// тип мапов WeakMap, который не сильно популярен и недооценен.
+// У них есть очень полезное свойство, которое позволяет классно реализовывать кеш.
 
-let wetherMap = new Map([
-  ['London', 10],
-  ['Moscow', 7],
-  ['Paris', 14],
-]);
+//методов у них меньше
+const map = new WeakMap();
 
-//мое решение
-// const wetherArr = [...wetherMap]
-// const newWetherArr = wetherArr.map(([city, tm]) => [tm, city])
-// const newWetherMap = new Map(newWetherArr)
+// ключи - только объекты или массив
+// map.set(1, 'test') //Error
 
+let a = { a: 1 }
+let b = { b: 2 }
+map.set(a, 'testA')
+map.set(b, 'testB')
+console.log(map.get(a))
+console.log(map) //WeakMap {{…} => 'testA', {…} => 'testB'}
 
-wetherMap = new Map([...wetherMap].map(el => el.reverse()))
-console.log(wetherMap);
+//weak отражается в том, что WeakMap хранит наш ключ до тех пор, пока ссылка на объект существует.
+// Например присвоем объекту "а" значение null(т.е. ссылки на оъект больше нет)
+a = null
+setTimeout(() => console.log(map), 1000) // WeakMap {{…} => 'testB'} через секунду key a исчез
+
+// но зачем нам  это самостоятельное удаление ? частое использование - это кеш
+let cache = new WeakMap();
+
+function getValue(obj) {
+  if (!cache.has(obj)) {
+    const res = 1; // очень долгое и дорогостоящее вычисление
+    cache.set(obj, res)
+  }
+  return cache.get(obj)
+}
+
+// в первом вызове вы заполним кеш и проведем дорогостоящуу операцию
+const result = getValue(b)
+// во втором уже возьмуться данные из кеша и операция производиться повторно не будет
+const result2 = getValue(b)
+console.log(result);
+console.log(result2);
+// и если мы обнулим наш 'b' объект, то кеш сам очиститься. нам не нужно думать об очистке кеша
