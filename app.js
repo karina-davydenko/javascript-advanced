6d695c0e84cd8287f30c8a1007e2331ca7b69a48
Karina Davydenko <kada61100@gmail.com>
2023-10-02 02:35:04 +0300

    Точность таймеров

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 'use strict'
 
-// Таймеры это не сама фича JS, это опять тваки API, которой мы имеей доступ,
-// работая в браузере или в nodej.
-// При этом работа таймера связанна с тем, как работает EventLoop.
-// Нам нужны таймеры для того чтобы отложить выполнение какой-то операции.
-
-
-const bashTimer = setTimeout((message) => {
-  console.log(message)
-}, 2000, 'Bash!')
-
-console.log(1)
+const mark1 = performance.now(); // число милисекунд с ачала запуска приложения
 
 setTimeout(() => {
-  console.log(2)
-}, 2000)
+  const mark2 = performance.now()
+  console.log(mark2 - mark1)
+}, 1000)
 
-clearTimeout(bashTimer)
-
-console.log(3)
