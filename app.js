6d6b4ddfb99a2f4215d53bd3236ebf328022c213
Karina Davydenko <kada61100@gmail.com>
2023-10-04 15:17:44 +0300

    Упражнение - Корзина товаров

--- app.js
+++ app.js
@@ -1,36 +1,64 @@
 'use strict'
 
-// Как работать с прототипами.
+/*
+  Реализовать на функциях и прототипах корзину товаров с методами
+  - Добавить товар
+  - Увеличить число товаров
+  - Уменьшить число товаров (удалить если их 0)
+*/
 
-const Book = function (title, author) {
-  this.title = title;
-  this.author = author;
-  this.isRead = false;
+
+const product = { id: 1, name: 'Bread', count: 1 }
+const product2 = { id: 2, name: 'Milk', count: 8 }
+
+const Card = function () {
+  this.products = []
 }
 
-Book.prototype.read = function () {
-  this.isRead = true
+Card.prototype.addProduct = function (product) {
+  if (this.products.find((p) => p.id === product.id)) {
+    return 'Товар уже добавлен в корзину.'
+  }
+  this.products.push(product)
+  return this
 }
 
-Book.prototype.cover = 'Paper'
+Card.prototype.increaseAmount = function (id) {
+  this.products = this.products.map((p) => {
+    if (p.id === id) {
+      p.count++
+      return p
+    }
+    return p
+  })
+}
 
-const lordOfTheRing = new Book('Lord of the Ring', 'Tolkien')
-console.log(lordOfTheRing) // Book {title: 'Lord of the Ring', author: 'Tolkien', isRead: false}
-lordOfTheRing.read()
-console.log(lordOfTheRing) // Book {title: 'Lord of the Ring', author: 'Tolkien', isRead: true}
-console.log(lordOfTheRing.cover) // Paper
-console.log(lordOfTheRing.hasOwnProperty('cover')) // false
-console.log(lordOfTheRing.hasOwnProperty('author')) // true
+Card.prototype.decreaseAmount = function (id) {
+  const indexProduct = this.products.findIndex((p) => p.id === id)
+  const product = this.products[indexProduct]
+  if (indexProduct === -1) {
+    return 'В корзине такого продукта нет'
+  }
+  if (product.count === 1) {
+    this.products.splice(indexProduct, 1)
+    return
+  }
+  this.products = this.products.map((p) => {
+    if (p.id === id) {
+      p.count--
+      return p
+    }
+    return p
+  })
 
+}
 
-console.log(lordOfTheRing.__proto__) // {read: ƒ, constructor: ƒ}
-console.log(lordOfTheRing.__proto__ === Book.prototype) // true
-console.log(Book.prototype.isPrototypeOf(lordOfTheRing)) // true
-console.log(Book.prototype.isPrototypeOf(Book)) // false
-
-
-// желательно не трогать прототипы встроенных объектов
-// такое поведение меняет внутренее устройство массивов
-Array.prototype.first = function () { }
-const a = [5]
-a.first
+const card = new Card()
+console.log(card)
+console.log(card.addProduct(product))
+console.log(card.addProduct(product))
+console.log(card.addProduct(product2))
+console.log(card.increaseAmount(1), card)
+console.log(card.decreaseAmount(1), card)
+console.log(card.decreaseAmount(1), card)
+console.log(card.decreaseAmount(1), card)
