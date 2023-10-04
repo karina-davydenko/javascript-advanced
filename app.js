34ec773c650b277ffea2e0490cd26b80befee01d
Karina Davydenko <kada61100@gmail.com>
2023-10-05 00:02:27 +0300

    Static

--- app.js
+++ app.js
@@ -1,62 +1,41 @@
 'use strict'
 
-const task = {
-  title: 'Tasl1',
-  dueTo: new Date('2023/01/01'),
+Number.MAX_SAFE_INTEGER // статичное числовое свойство
+new Number()
 
-  isOverdue() {
-    return this.dueTo < new Date()
-  }
+Array.from([0, 1, 2]) // статичный метод
+new Array()
 
-}
-
-console.log(task.isOverdue())
-
-// get/set - возможноность использовать методы, при этом вызывая как свойства.
-
-const task1 = {
-  title: 'Task1',
-  dueTo: new Date('2023/01/01'),
-
-  get isOverdue() {
-    return this.dueTo < new Date()
-  },
-
-  set isOverdue(isOverdueTask) {
-    if (!isOverdueTask) {
-      this.dueTo = new Date()
-    }
+// с инициализацией теста
+class Test {
+  hello() {
+    console.log('hello')
   }
 }
+const test = new Test()
+test.hello()
 
-// console.log(task1.isOverdue()) // TypeError
-console.log(task1.isOverdue) // true
-console.log(task1)
-task1.isOverdue = false
-console.log(task1)
+// без  инициализации теста, используем static метод
+class Test1 {
+  static a = 1
 
-// Классы
-class Task {
-  constructor(title, dueDate) {
-    this.title = title;
-    this.dueDate = dueDate;
+  static hello1() {
+    console.log('hello1')
   }
 
-  get isOverdue() {
-    return this.dueDate < new Date()
-  }
-
-  set dueDate(date) {
-    // валидация на дату подействует и при создании объекта 
-    if (date < new Date()) {
-      return
-    }
-    // this.dueDate = date //рекурсивный вызов set привидет к переполнению стека
-    this._dueDate = date // использовать свойсто с нижним подчеркиванием
+  static {
+    let b = 5;
+    this.a++
+    b++
   }
 }
+Test1.hello1()
+console.log(Test1.a)
 
-const newTask = new Task('Task2', new Date('2023/03/03'))
-console.log(newTask)
-newTask.dueDate = new Date('2024/03/03')
-console.log(newTask)
+
+// Под капотом
+const Test2 = function () { }
+Test2.hello2 = function () {
+  console.log('hello2')
+}
+Test2.hello2()
