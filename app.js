c099ec52b323a217e80179b5af9a7141e290e113
Karina Davydenko <kada61100@gmail.com>
2023-10-04 15:42:53 +0300

    Цепочка прототипов

--- app.js
+++ app.js
@@ -1,64 +1,25 @@
 'use strict'
 
-/*
-  Реализовать на функциях и прототипах корзину товаров с методами
-  - Добавить товар
-  - Увеличить число товаров
-  - Уменьшить число товаров (удалить если их 0)
-*/
+/**
+ * Процесс создания с new
+ *  - Путой объект
+ *  - Вызов конструктора
+ *  - Связывание с Prototype
+ *  - Возврат объекта
+ */
 
-
-const product = { id: 1, name: 'Bread', count: 1 }
-const product2 = { id: 2, name: 'Milk', count: 8 }
-
-const Card = function () {
-  this.products = []
+const Book = function (title, author) {
+  this.title = title;
+  this.author = author;
 }
 
-Card.prototype.addProduct = function (product) {
-  if (this.products.find((p) => p.id === product.id)) {
-    return 'Товар уже добавлен в корзину.'
-  }
-  this.products.push(product)
-  return this
-}
+Book.prototype.isRead = false
 
-Card.prototype.increaseAmount = function (id) {
-  this.products = this.products.map((p) => {
-    if (p.id === id) {
-      p.count++
-      return p
-    }
-    return p
-  })
-}
+const lordOfTheRing = new Book('LordOfTheRing', 'Author')
 
-Card.prototype.decreaseAmount = function (id) {
-  const indexProduct = this.products.findIndex((p) => p.id === id)
-  const product = this.products[indexProduct]
-  if (indexProduct === -1) {
-    return 'В корзине такого продукта нет'
-  }
-  if (product.count === 1) {
-    this.products.splice(indexProduct, 1)
-    return
-  }
-  this.products = this.products.map((p) => {
-    if (p.id === id) {
-      p.count--
-      return p
-    }
-    return p
-  })
+console.log(lordOfTheRing)
+console.log(lordOfTheRing.__proto__.__proto__.__proto__)
+console.log(lordOfTheRing.__proto__.__proto__)
 
-}
-
-const card = new Card()
-console.log(card)
-console.log(card.addProduct(product))
-console.log(card.addProduct(product))
-console.log(card.addProduct(product2))
-console.log(card.increaseAmount(1), card)
-console.log(card.decreaseAmount(1), card)
-console.log(card.decreaseAmount(1), card)
-console.log(card.decreaseAmount(1), card)
+console.log(Book.prototype)
+console.log(Book.prototype.__proto__); //Методы касающиеся объекта
