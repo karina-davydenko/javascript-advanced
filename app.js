237eab2b0eafed7917c1aa424133931142a3d3af
Karina Davydenko <kada61100@gmail.com>
2023-10-15 16:18:08 +0300

    Принцип открытости и закрытости

--- app.js
+++ app.js
@@ -1,36 +1,65 @@
 'use strict'
 
-// Принцип единой ответственности - наш класс должен делать что-то одно,
-// но хорошо!
+// Принцип открытости и закрытости - наш классдолжен быть открыт к дополнению,
+// но закрыт для модификаций
+// Т.е. когда мы хотим расширить поведение нашего класса, мы это можем сделать, 
+// но не путем изменения самого класса внутри
 
-class Charcter {
-  #inventory = []
-  #health = 10
+// Как не надо
+// class Treasure {
 
-  pickItem(item) {
-    this.#inventory.push(item)
-  }
+// }
 
-  recieveDamage(damage) {
-    this.#health -= damage
-  }
+// class Coin extends Treasure {
 
-  //  это не ответсвенность персонажа сохранение, надо рагпузить
-  // saveCharacter() {
-  //   localStorage.setItem('char', this)
-  // }
+// }
 
-  // loadCharacter() {
-  //   //...
-  // }
+// class Crystal extends Treasure {
+
+// }
+
+// class Brilliant extends Treasure {
+
+// }
+
+// // Теперь мы захотели реализовать класс инвентаря
+// class Inventory {
+//   #score;
+//   pick(treasure) {
+//     if (treasure instanceof Coin) {
+//       this.#score += 1
+//     }
+//     if (treasure instanceof Crystal) {
+//       this.#score += 10
+//     }
+
+//   }
+// }
+// Такая реализация нарушает принцип открытости/закрытости.
+// Вот мы добавляем новый частный класс бриллиант, и дополнительно приходиться
+// править код в инвенторе, чтобы добавить проверку на бриллианты
+
+
+// Как надо
+class Treasure {
+  value = 0
 }
 
-class DB {
-  save(item) {
-    localStorage.setItem('char', item)
-  }
+class Coin extends Treasure {
+  value = 1
+}
 
-  load() {
-    //...
+class Crystal extends Treasure {
+  value = 10
+}
+
+class Brilliant extends Treasure {
+  value = 20
+}
+
+class Inventory {
+  #score;
+  pick(treasure) {
+    this.#score += treasure.value
   }
 }
