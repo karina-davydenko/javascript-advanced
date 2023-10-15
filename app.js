92572bc5a3966cedf3173e1da2a0496b02e1c671
Karina Davydenko <kada61100@gmail.com>
2023-10-15 11:02:21 +0300

    Упражение - Удар по орку

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
 'use strict'
+/**
+ * Сделать класс врага со здоровьем и методом полученияя урона
+ * Сделать класс меча, который имеет силу и метод нанесение урона
+ * Сделать класс орка,, который в 50% случаев не получает урон
+ */
 
-class Book {
-  constructor(title, author) {
-    this.title = title
-    this.author = author
+class Enemy {
+  constructor(hp) {
+    this.hp = hp;
   }
 
-  info() {
-    console.log(`${this.title} - ${this.author}`)
-  }
-}
-
-const book1 = new Book('Title1', 'Author1')
-book1.info()
-
-class EBook extends Book {
-  constructor(title, author, pages) {
-    super(title, author)
-    this.pages = pages
-  }
-
-  info() {
-    console.log(`${this.title} - ${this.author} - ${this.pages}`)
+  _damageEmeny(force) {
+    this.hp = this.hp - force
   }
 }
 
-const book2 = new EBook('Title2', 'Author2', 100)
-book2.info()
+class Sword {
+  constructor(force) {
+    this.force = force;
+  }
+
+  attack(enemy) {
+    enemy._damageEmeny(this.force)
+  }
+}
+
+class Ork extends Enemy {
+  constructor(hp) {
+    super(hp);
+    this.hit = true;
+  }
+
+  _damageEmeny(force) {
+    if (Math.random() > 0.5) {
+      this.hp = this.hp - force
+    }
+  }
+}
+
+const enemy = new Enemy(50);
+const ork = new Ork(50);
+const sword = new Sword(10)
+console.log(enemy)
+console.log(ork)
+console.log(sword)
+sword.attack(enemy)
+sword.attack(ork)
+sword.attack(ork)
