85b53a1ee06569f96a97751561f09f9b39b952a9
Karina Davydenko <kada61100@gmail.com>
2023-10-15 12:09:47 +0300

    Полиморфизм

--- app.js
+++ app.js
@@ -1,49 +1,18 @@
 'use strict'
 /**
- * Сделать класс врага со здоровьем и методом полученияя урона
- * Сделать класс меча, который имеет силу и метод нанесение урона
- * Сделать класс орка,, который в 50% случаев не получает урон
+ * Виды полиморфизма
+ * - Ad-hock полиморфизм
+ *    Возможность по разному исполнять функцию в зависимости от типа данных
+ *    Пример:
+ *    "2" + "4" (конкатенация)
+ *    2 + 4 (сложение)
+ * 
+ * - Параметричский полиморфизм
+ *    Когда мы можем выполнять одну и ту же функцию, но с разным типом аргументов
+ *    Пример:
+ *    console.log(1)
+ *    console.log('str') и т.д.
+ * 
+ * Виды полиморфизма выше не совсем относятся к ООП. Чаще имеется в виду полиморфизм подтипов.
+ * - Полиморфизм подтипов
  */
-
-class Enemy {
-  constructor(hp) {
-    this.hp = hp;
-  }
-
-  _damageEmeny(force) {
-    this.hp = this.hp - force
-  }
-}
-
-class Sword {
-  constructor(force) {
-    this.force = force;
-  }
-
-  attack(enemy) {
-    enemy._damageEmeny(this.force)
-  }
-}
-
-class Ork extends Enemy {
-  constructor(hp) {
-    super(hp);
-    this.hit = true;
-  }
-
-  _damageEmeny(force) {
-    if (Math.random() > 0.5) {
-      this.hp = this.hp - force
-    }
-  }
-}
-
-const enemy = new Enemy(50);
-const ork = new Ork(50);
-const sword = new Sword(10)
-console.log(enemy)
-console.log(ork)
-console.log(sword)
-sword.attack(enemy)
-sword.attack(ork)
-sword.attack(ork)
