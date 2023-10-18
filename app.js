0dda6de2127b5c6429f0f9d37d0b519086e5a6fb
Karina Davydenko <kada61100@gmail.com>
2023-10-19 00:58:30 +0300

    Callback Hall

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 'use strict'
 
-/**
- * Получить среднюю цену 30 товаров из API
- * https://dummyjson.com/products
- */
-
-// создаем новый екземпляр XMLHttpRequest
 const req = new XMLHttpRequest()
-//сконфигурировать(не отправлять) url 
 req.open('GET', 'https://dummyjson.com/products')
-// отправляем запрос
 req.send()
 
+
+// Попытка сделать последовательную цепочку запросов приводит к плохо читаемому коду
+// Callback Hall
 req.addEventListener('load', function () {
   const { products } = JSON.parse(this.responseText)
   const sum = products.reduce((acc, product) => {
     return acc += product.price
   }, 0)
   console.log(sum / products.length)
+  console.log(products)
+
+  const req = new XMLHttpRequest()
+  req.open('GET', 'https://dummyjson.com/products/' + products[0].id)
+  req.send()
+
+  req.addEventListener('load', function () {
+    const product = JSON.parse(this.responseText)
+    console.log(product)
+
+    const req = new XMLHttpRequest()
+    req.open('GET', 'https://dummyjson.com/products/' + products[1].id)
+    req.send()
+
+    req.addEventListener('load', function () {
+      const product = JSON.parse(this.responseText)
+      console.log(product)
+    })
+  })
 })
 
