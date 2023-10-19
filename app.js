5bee5e9ee098e1b9da1bccf2a64a5e5bab67e92b
Karina Davydenko <kada61100@gmail.com>
2023-10-19 15:00:30 +0300

    Finally

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict'
 
-fetch('https://dummyjson.com/products')
+const API = 'https://dummyjson.com/products/'
+fetch(API)
   .then(res => res.json())
   .then(({ products }) => {
     console.log(products);
-    return fetch('https://dummyjson.com/product/' + products[0].id)
+    return fetch(API + products[0].id)
   })
   .then(res => res.json())
   .then(data => console.log(data))
   .catch(err => console.log(err))
+  .finally(() => {
+    console.log('Finally')
+  })
