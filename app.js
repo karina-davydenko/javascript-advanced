e325b8a66207e2e60750107aa812ccb8848cd95b
Karina Davydenko <kada61100@gmail.com>
2023-10-19 14:44:39 +0300

    Цепочка прописов

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 'use strict'
 
-fetch('https://dummyjson.com/products/1')
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+// Осторожно с колбеками в промиссах. Так делать не надо.
+// fetch('https://dummyjson.com/products')
+//   .then((response) => response.json())
+//   .then(({ products }) => {
+//     console.log(products);
+//     fetch('https://dummyjson.com/product/' + products[0].id)
+//       .then(res => res.json())
+//       .then(data => console.log(data))
+//   })
+
+fetch('https://dummyjson.com/products')
+  .then(res => res.json())
+  .then(({ products }) => {
+    console.log(products);
+    return fetch('https://dummyjson.com/product/' + products[0].id)
   })
+  .then(res => res.json())
+  .then(data => console.log(data))
