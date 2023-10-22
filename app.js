3f493dcc4cf55e5200a9e1c4ea848989c9bb49c9
Karina Davydenko <kada61100@gmail.com>
2023-10-23 01:02:02 +0300

    Упражнение - Мой город

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 'use strict'
 
-// function getProgects() {
-//   fetch('https://dummyjson.com/products')
-//     .then(res => res.json())
-//     .then(console.log);
-// }
+/**
+ * Получить геологацию пользователя через
+ * Geolocation.getCurrentPosition() (WEB API) и по координатам
+ * определить город, отправив запрос:
+ * https://api.bigdatacloud.net/data/reverse-geocode-client?
+ * latitude=37.42159&longitude=-122.0837&localityLanguage=en
+ */
 
-// getProgects()
-// console.log('end')
-
-async function getProgects() {
-  try {
-    const productsRes = await fetch('https://dummyjson.com/products')
-    if (!productsRes.ok) {
-      throw new Error(productsRes.status + productsRes.statusText)
+function getCoordinates() {
+  return new Promise((resolve, reject) => {
+    function success(pos) {
+      const { coords } = pos;
+      resolve({ latitude: coords.latitude, longitude: coords.longitude })
     }
-    const { products } = await productsRes.json();
-    console.log(products)
 
-    const productRes = await fetch('https://dummyjson.com/products/' + products[0].id)
-    const product = await productRes.json();
-    console.log(product)
-  } catch (err) {
-    console.log(err)
-  } finally {
-    console.log('Finished')
-  }
+    function error(err) {
+      reject(new Error(`(${err.code}): ${err.message}`))
+    }
 
+    navigator.geolocation.getCurrentPosition(success, error)
+  })
 }
 
-getProgects()
-console.log('end')
+async function getCity() {
+  try {
+    const { latitude, longitude } = await getCoordinates()
+    const res = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`)
+    if (res.status !== 200) {
+      throw new Error(`${res.status} ${res.statusText}`)
+    }
+    const { city } = await res.json()
+    console.log(city)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+getCity()
+
+
+
+
 
