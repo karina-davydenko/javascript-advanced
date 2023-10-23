75fbed2458967116225482ed42b5d4ae09108d22
Karina Davydenko <kada61100@gmail.com>
2023-10-23 15:26:39 +0300

    Асинхронные методы и последовательное выполнение

--- app.js
+++ app.js
@@ -1,43 +1,35 @@
 'use strict'
 
-/**
- * Получить геологацию пользователя через
- * Geolocation.getCurrentPosition() (WEB API) и по координатам
- * определить город, отправив запрос:
- * https://api.bigdatacloud.net/data/reverse-geocode-client?
- * latitude=37.42159&longitude=-122.0837&localityLanguage=en
- */
-
-function getCoordinates() {
-  return new Promise((resolve, reject) => {
-    function success(pos) {
-      const { coords } = pos;
-      resolve({ latitude: coords.latitude, longitude: coords.longitude })
-    }
-
-    function error(err) {
-      reject(new Error(`(${err.code}): ${err.message}`))
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error)
-  })
-}
-
-async function getCity() {
-  try {
-    const { latitude, longitude } = await getCoordinates()
-    const res = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`)
-    if (res.status !== 200) {
-      throw new Error(`${res.status} ${res.statusText}`)
-    }
-    const { city } = await res.json()
-    console.log(city)
-  } catch (e) {
-    console.error(e)
+class ProductRepository {
+  async getProducts() {
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json()
+    console.log(data)
   }
 }
 
-getCity()
+const repo = new ProductRepository()
+// repo.getProducts()
+
+const asyncArrow = async () => {
+  try {
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
+
+
+(async () => {
+  console.log(1)
+  const re = await asyncArrow()
+  console.log(re)
+  console.log(2)
+})()
+
 
 
 
