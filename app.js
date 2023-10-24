8d4b38666258e31988cd75e5e88984a7dcc7b3e1
Karina Davydenko <kada61100@gmail.com>
2023-10-25 01:52:29 +0300

    Возможности fetch

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
 'use strict'
-async function getProduct(id) {
-  const res = await fetch('https://dummyjson.com/products/' + id)
-  return res.json()
-}
-
-async function getProductError(id) {
-  const res = await fetch('https://dummyjsons.com/products/' + id)
-  return res.json()
-}
 
 async function main() {
-  const res1 = await Promise.all([
-    getProduct(1),
-    getProduct(2),
-  ])
-  console.log(res1)
+  const res = await fetch('https://dummyjson.com/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      username: "atuny0",
+      password: "9uQFF1Lh",
+    })
+  })
 
-  const err1 = await Promise.all([
-    getProduct(1),
-    // getProductError(2),
-  ])
-  console.log(err1)
-
-  const res2 = await Promise.allSettled([
-    getProduct(1),
-    getProduct(2),
-  ])
-  console.log(res2)
-  const err2 = await Promise.allSettled([
-    getProductError(1),
-    getProduct(2),
-  ])
-  console.log(err2)
+  const data = await res.json()
+  console.log(data)
 }
 
-main()
+main()  
