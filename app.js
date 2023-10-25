eeb2da703154fe3cf49eedfbc2e372781ee128bd
Karina Davydenko <kada61100@gmail.com>
2023-10-25 14:27:27 +0300

    Упражнение - Генератор активностей

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 'use strict'
 
-async function main() {
-  const res = await fetch('https://dummyjson.com/auth/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      username: "atuny0",
-      password: "9uQFF1Lh",
-    })
-  })
+/**
+ * Сделать генератор 3х идей от скуки
+ * http://www.boredapi.com/api/activity/
+ * с отображением на странице
+ */
 
+const button = document.querySelector('.random-btn')
+const container = document.querySelector('.container-card')
+
+async function random() {
+  const res = await fetch('http://www.boredapi.com/api/activity/')
   const data = await res.json()
-  console.log(data)
+  return data.activity
 }
 
-main()  
+async function getRandomEvents() {
+  const res = await Promise.all([
+    random(),
+    random(),
+    random(),
+  ])
+
+  container.innerHTML = res.map(str => {
+    return `
+      <div class="card-random-event">
+        <p>
+          ${str}
+        </p>
+      </div>
+    `
+  }).join('')
+}
+
+
+button.addEventListener('click', () => {
+  getRandomEvents()
+})
