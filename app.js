fba81406bcf793c3d56e62a1f1af288fd3564d29
Karina Davydenko <kada61100@gmail.com>
2023-10-28 14:14:48 +0300

    Делегирование событий

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 'use strict'
 
-// через addEventListener можно добавлять нескл событий, в отличии от навешивания через html
-const button = document.querySelector('.button');
-const inner = document.querySelector('.inner');
 const wrapper = document.querySelector('.wrapper');
 
-button.addEventListener('click', function (e) {
-  console.log(e.target)
-  console.log(e.currentTarget)
-  this.style.backgroundColor = 'purple'
+for (let i = 0; i < 100; i++) {
+  const el = document.createElement('div')
+  el.innerHTML = `Пользователь с id: ${i}`
+  el.setAttribute('data-id', i)
+  // el.addEventListener('click', () => {
+  //   console.log(`Deleted user ${i}`)
+
+  // })
+  wrapper.appendChild(el)
+}
+
+wrapper.addEventListener('click', (e) => {
+  const i = e.target.getAttribute('data-id')
+  console.log(`Deleted user ${i}`)
+
 })
-inner.addEventListener('click', function (e) {
-  console.log(e.target)
-  console.log(e.currentTarget)
-  this.style.backgroundColor = 'blue'
-  // остановка всплытия
-  e.stopPropagation()
-})
-wrapper.addEventListener('click', function (e) {
-  console.log(e.target)
-  console.log(e.currentTarget)
-  this.style.backgroundColor = 'green'
-}, true)
 
 
 
