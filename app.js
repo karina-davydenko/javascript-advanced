5dc164c5ad5fa926721c98d5da9d5088b65bf0be
Karina Davydenko <kada61100@gmail.com>
2023-11-06 15:50:45 +0300

    Упражнение - Поиск по списку

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 'use strict'
 
-// когда html распарсилась и сгенерился дом
-document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('DOMContentLoaded', e);
+/**
+ * Динамически создать N элементов с текстом и поле для поиска.
+ * При вводе в поле, выделять элементы, которые содержат введенный текст.
+ */
+
+const wrapper = document.querySelector('.wrapper')
+for (let i = 1; i <= 30; i++) {
+  const el = document.createElement('div')
+  el.innerHTML = `User ${i}`
+  wrapper.append(el)
+}
+
+const listUsers = [...wrapper.children]
+const input = document.querySelector('input');
+input.addEventListener('change', function (event) {
+  const { value } = event.target
+  listUsers.forEach((user) => {
+    user.classList.remove('active')
+    if (user.innerHTML.includes(value)) {
+      user.classList.add('active')
+    }
+    return
+  })
 })
 
-// когда все ресурсы загружены на стр
-window.addEventListener('load', function (e) {
-  console.log('load', e);
-})
-
-// когда надо придупредить пользователя о несохр данных
-window.addEventListener('beforeunload', function (e) {
-  e.preventDefault();
-  e.returnValue = ''
-})
