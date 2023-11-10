77712b43da14665a89015f0e32f5b669e59a7f90
Karina Davydenko <kada61100@gmail.com>
2023-11-10 21:43:04 +0300

    Верстка шапки

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,5 +1,6 @@
 import { AbstractView } from "../../common/view.js";
 import onChange from "on-change";
+import { Header } from "../../components/header/header..js";
 
 export class MainView extends AbstractView {
   state = {
@@ -23,10 +24,14 @@ export class MainView extends AbstractView {
 
   render() {
     const main = document.createElement('div');
-    main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
     this.app.innerHTML = '';
     this.app.append(main);
-    this.appState.favorites.push('1')
+    this.renderHeader()
 
+  }
+
+  renderHeader() {
+    const header = new Header(this.appState).render();
+    this.app.prepend(header);
   }
 }
