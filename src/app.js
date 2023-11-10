cd8d07c2b0640062e5b3e89dfeb0b7b089e5ff06
Karina Davydenko <kada61100@gmail.com>
2023-11-10 18:39:55 +0300

    State и реактивность

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ class App {
     { path: '', view: MainView }
   ];
 
+  appState = {
+    favorites: []
+  };
+
   constructor() {
     window.addEventListener('hashchange', this.route.bind(this));
     this.route();
@@ -15,9 +19,9 @@ class App {
       this.currentView.destroy()
     }
     const view = this.routes.find(r => r.path == location.hash).view;
-    this.currentView = new view();
+    this.currentView = new view(this.appState);
     this.currentView.render();
   }
 }
 
-new App();
+new App()
